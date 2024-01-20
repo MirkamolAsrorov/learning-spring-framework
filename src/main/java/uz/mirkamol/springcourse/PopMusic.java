329c890bb09e4a6006265b812557d5bdeab5d9fa
mirkamol <mirkamolasrorov5@gmail.com>
2024-01-20 16:48:13 +0500

    learned @PostConstruct and @PreDestroy anotations

--- src/main/java/uz/mirkamol/springcourse/PopMusic.java
+++ src/main/java/uz/mirkamol/springcourse/PopMusic.java
@@ -3,13 +3,18 @@ package uz.mirkamol.springcourse;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-
+@Component
 public class PopMusic implements Music{
+
+    @PostConstruct
     public void doMyInit(){
         System.out.println("Doing my initialization");
     }
 
+    @PreDestroy
     public void doMyDestroy(){
         System.out.println("Doing my destruction");
     }
